007b3e1cd134d60248d173a5d576a89919cc7f7f
Dija <71792144+khadijabaddouj@users.noreply.github.com>
2021-07-11 15:00:44 +0200

    jdk

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -5,7 +5,7 @@ export async function handler (req: object) {
   return {
     statusCode: 200,
     headers: {
-      'content-type': 'text/html; charset=utf8',
+      
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
       'crossOrigin': 'null'
     },
