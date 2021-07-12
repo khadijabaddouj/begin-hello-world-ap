bca3db4dce365649cd06c0b98fb798e02d658363
Dija <71792144+khadijabaddouj@users.noreply.github.com>
2021-07-12 10:01:44 +0200

    hdj

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -4,7 +4,7 @@ export async function handler (req: object) {
   return {
     statusCode: 200,
     headers: {
-       'content-type': 'application/json; charset=utf8',
+       'content-type': "application/json; charset=utf8",
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
       'Access-Control-Allow-Origin':'*',
     },
