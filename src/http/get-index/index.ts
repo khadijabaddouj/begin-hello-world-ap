4376728fedd67566ac3f34ad920b2c6962b0c034
Dija <71792144+khadijabaddouj@users.noreply.github.com>
2021-07-12 09:52:47 +0200

    ghh

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -4,10 +4,9 @@ export async function handler (req: object) {
   return {
     statusCode: 200,
     headers: {
+       'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
       'Access-Control-Allow-Origin':'*',
-      'Access-Control-Allow-Credentials':'true',
-      'Access-Control-Allow-Headers': 'X-Requested-With',
-      'Access-Control-Allow-Methods': 'POST ,GET'
     },
     body:JSON.stringify('hello')
   }
